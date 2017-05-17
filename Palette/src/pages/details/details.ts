3127d6c49ffc90d65389a57f0cabd707b2041cf4
oingbong <oingbong@gmail.com>
2017-05-17 23:02:17 +0900

    Change Title to Item Title

--- Palette/src/pages/details/details.ts
+++ Palette/src/pages/details/details.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
   templateUrl: 'details.html'
@@ -8,7 +8,8 @@ import { NavController } from 'ionic-angular';
   // (header 안 나오고 두번째 클릭시 나옴)
 })
 export class DetailsPage {
-  constructor(public navCtrl: NavController){
-
+  item: any;
+  constructor(public navCtrl: NavController, public params:NavParams){
+    this.item = params.get('item');
   }
 }
